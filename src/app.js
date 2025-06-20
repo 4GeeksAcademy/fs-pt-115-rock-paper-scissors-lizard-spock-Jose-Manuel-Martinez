3cb261f4da6170375ccb4826a95b3f6c2f181c8c
Jose <avu11@protonmail.com>
2025-06-20 19:50:45 +0000

    mañana termino y lo hago version web

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 //write your code here
-console.log("Hello Rigo from the console!");
+const opciones = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
+let usuario = 'rock'; 
+
+let computadora = opciones[Math.floor(Math.random() * opciones.length)];
+
+function determinarGanador(jugador, maquina) {
+  const reglas = {
+    rock: ['scissors', 'lizard'],
+    paper: ['rock', 'spock'],
+    scissors: ['paper', 'lizard'],
+    lizard: ['paper', 'spock'],
+    spock: ['scissors', 'rock']
+  };
+
+  if (jugador === maquina) return '🤘Empate!🤘';
+  if (reglas[jugador].indexOf(maquina) !== -1) return '🚀Ganaste!🚀';
+  return '🫷Perdiste!🫸';
+}
+
+console.log("Elegiste:", usuario);
+console.log("CPU eligió:", computadora);
+console.log(determinarGanador(usuario, computadora));
